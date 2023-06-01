bd762c09e2a256bd2b86679a6e4a77f1473316c7
Loyd <loydqre@gmail.com>
2023-06-01 23:55:06 +0530

    first release of project

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnEl = document.getElementsByClassName('.btn');
+
+btnEl.addEventListener('click', () => {
+    console.log('cccc');
+    // const bodyBg = document.getElementsByTagName('body')
+    // chars = '0123456789abcdef';
+    // colorCodeLen = 6;
+    // colorCode = "";
+    // for (let index = 0; index < colorCodeLen; index++) {
+    //     const randomNum = Math.floor(Math.random() * chars.length);
+    //     colorCode += chars.substring(randomNum, randomNum + 1);
+    // }
+    // bodyBg.style.backgroundColor = `# + ${colorCode}`
+})
